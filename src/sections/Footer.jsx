6b617b869834959e1a34c4c0131ac64c19f86b0d
Ayodele Temitope <dyenamics228@gmail.com>
2024-11-12 02:55:12 +0100

    add icons to footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,6 +2,8 @@ import logo from "../assets/ani-logo.jpg";
 import telegram from "../assets/telegram.svg";
 import solana from "../assets/solana-sol-logo.svg";
 // import twitter from "../assets/twitter.svg";
+import binance from "../assets/binance-logo.svg";
+import cmc from "../assets/coinmarketcap-1.svg";
 import { FaXTwitter } from "react-icons/fa6";
 import { motion } from "framer-motion";
 
@@ -37,26 +39,30 @@ const Footer = () => {
           </li> */}
         </ul>
         <div className="flex justify-center gap-4 items-center mt-6">
-          <motion.img
+          <img src={binance} alt="" width={70} height={70} />
+          <motion.a
             initial="hidden"
             whileInView="show"
             variants={fadeIn("right", "tween", 0.5, 0.5)}
             viewport={{ once: true }}
-            src={telegram}
-            alt=""
-            width={50}
-            height={50}
-          />
-          <img src={solana} alt="" width={50} height={50} />
+            href="https://t.me/aniborgsOnSol"
+          >
+            <img src={telegram} alt="" width={70} height={70} />
+          </motion.a>
+          <a href="">
+            <img src={solana} alt="" width={70} height={70} />
+          </a>
+
           <motion.a
             initial="hidden"
             whileInView="show"
             variants={fadeIn("left", "tween", 0.5, 0.5)}
             viewport={{ once: true }}
-            href="https://x.com/anonomize?s=21"
+            href=""
           >
             <FaXTwitter className="text-6xl" />
           </motion.a>
+          <img src={cmc} alt="" width={70} height={70} />
         </div>
       </nav>
     </footer>
