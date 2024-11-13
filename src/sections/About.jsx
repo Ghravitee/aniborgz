1754bd315d5319277860dfed81abbff1d5cb0cb9
Ayodele Temitope <dyenamics228@gmail.com>
2024-11-13 03:19:10 +0100

    remove links in about

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,13 +1,7 @@
 import ani2 from "../assets/ani-2.webp";
-import telegram from "../assets/telegram.svg";
-import solana from "../assets/solana-sol-logo.svg";
-// import twitter from "../assets/twitter.svg";
-import binance from "../assets/binance-logo.svg";
-import cmc from "../assets/coinmarketcap-1.svg";
 import { motion } from "framer-motion";
-import { FaXTwitter } from "react-icons/fa6";
 
-import { fadeIn, zoomIn } from "../utils/motion";
+import { zoomIn } from "../utils/motion";
 
 const About = () => {
   return (
@@ -51,32 +45,6 @@ const About = () => {
             Borgs bridges Solana and Cosmos, creating a seamless multi-chain
             environment that enhances asset portability and user experience.
           </p>
-          <div className="flex justify-center gap-4 items-center mt-6">
-            <img src={binance} alt="" width={70} height={70} />
-            <motion.a
-              initial="hidden"
-              whileInView="show"
-              variants={fadeIn("right", "tween", 0.5, 0.5)}
-              viewport={{ once: true }}
-              href="https://t.me/aniborgsOnSol"
-            >
-              <img src={telegram} alt="" width={70} height={70} />
-            </motion.a>
-            <a href="">
-              <img src={solana} alt="" width={70} height={70} />
-            </a>
-
-            <motion.a
-              initial="hidden"
-              whileInView="show"
-              variants={fadeIn("left", "tween", 0.5, 0.5)}
-              viewport={{ once: true }}
-              href="https://x.com/aniborgsxyz"
-            >
-              <FaXTwitter className="text-6xl" />
-            </motion.a>
-            <img src={cmc} alt="" width={70} height={70} />
-          </div>
         </div>
       </div>
       <h2 className="unlock text-[1.7rem] my-8">Architectural Overview</h2>
